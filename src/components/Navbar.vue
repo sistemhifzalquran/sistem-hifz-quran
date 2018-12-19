d17b19e0865dee1eba635417fa49ff8a4b857424
kamil <kamilzobri@kamils-MacBook-Air.local>
2018-12-19 23:01:55 +0800

    add drawer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <v-toolbar app>
-        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title>Hifz AlQuran</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn flat color="grey">
@@ -9,9 +9,16 @@
         <v-icon right>exit_to_app</v-icon>
       </v-btn>
     </v-toolbar>
+    <v-navigation-drawer app v-model="drawer"></v-navigation-drawer>
   </nav>
 </template>
 
 <script>
-export default {};
+export default {
+  data(){
+    return{
+      drawer: false
+    }
+  }
+};
 </script>
