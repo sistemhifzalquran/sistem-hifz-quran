30d40cbbead8b9d22ed8bd950ab90d380cac4e80
kamil <kamilzobri@kamils-MacBook-Air.local>
2018-12-19 21:08:03 +0800

    add navbar n tool bar..horayyy

---
+++ src/components/Admin.vue
@@ -0,0 +1,15 @@
+<template>
+  <Navbar />
+</template>
+
+<script>
+import Navbar from '@/components/Navbar'
+export default {
+components:{Navbar},
+data(){
+  return{
+    
+  }
+}
+};
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav>
+    <v-toolbar app>
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title>Hifz AlQuran</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat color="grey">
+        <span>Sign Out</span>
+        <v-icon right>exit_to_app</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
