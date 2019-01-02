753ceca725bd65a00af787f6a3f9b246c4a7aa79
kamil <kamilzobri@kamils-MacBook-Air.local>
2019-01-02 23:57:59 +0800

    menu daftar student and validation n keypress guard

--- src/components/Students.vue
+++ src/components/Students.vue
@@ -1,5 +1,45 @@
 <template>
   <v-container align-content-center>
+    <v-menu offset-y>
+      <v-btn flat slot="activator" color="grey">
+        <v-icon left>expand_more</v-icon>
+        <span>Urus Pelajar</span>
+      </v-btn>
+      <v-list>
+        <v-dialog max-width="600px" v-model="dialog">
+          <v-list-tile slot="activator">
+            <v-list-tile-title prepend-icon="person_add">Daftar Pelajar Baru</v-list-tile-title>
+          </v-list-tile>
+          <v-card>
+            <v-card-title>
+              <h2>Daftar Pelajar Baru</h2>
+            </v-card-title>
+            <v-card-text>
+              <v-form class="text-xs-right" ref="formNewStudent">
+                <v-text-field
+                  @keypress="onlyLetter"
+                  :rules="inputRulesNewStudent"
+                  label="Nama Pelajar"
+                  v-model="signupForm.name"
+                  maxlength="100"
+                ></v-text-field>
+                <v-text-field
+                  @keypress="onlyNumber"
+                  :rules="inputRulesIC"
+                  label="No Kad Pengenalan Pelajar"
+                  v-model="signupForm.iC"
+                  maxlength="12"
+                ></v-text-field>
+                <v-btn
+                  @click="registerNewStudent()"
+                  :loading="performingRequest ? true : false"
+                >Hantar</v-btn>
+              </v-form>
+            </v-card-text>
+          </v-card>
+        </v-dialog>
+      </v-list>
+    </v-menu>
     <v-expansion-panel popout>
       <v-expansion-panel-content flat v-for="student in studentDataList" :key="student.ic">
         <v-layout slot="header" row wrap class="pa-1">
@@ -35,12 +75,50 @@ import { mapState } from "vuex";
 //const fb = require("../firebaseConfig.js");
 export default {
   data() {
-    return {};
+    return {
+      signupForm: {
+        name: "",
+        iC: ""
+      },
+      performingRequest: false,
+      dialog: false,
+      inputRulesNewStudent: [
+        v =>
+          (v.length >= 8 && v.length <= 100) ||
+          "Masukkan Nama termasuk bin atau binti",
+        v => /^([a-zA-Z]+\s)*['. a-zA-Z]+$/.test(v) || "Pastikan huruf Sahaja"
+      ],
+      inputRulesIC: [
+        v => v.length == 12 || "Pastikan Kad Pengenalan mencukupi",
+        v => /^\d+$/.test(v) || "Pastikan Nombor Sahaja"
+      ]
+    };
   },
   computed: {
     ...mapState(["studentDataList", "currentGroup", "noStudent"])
   },
-  methods: {}
+  methods: {
+    registerNewStudent() {},
+    onlyNumber($event) {
+      let keyCode = $event.keyCode ? $event.keyCode : $event.which;
+      if ((keyCode < 48 || keyCode > 57) && keyCode !== 46) {
+        // 46 is dot
+        $event.preventDefault();
+      }
+    },
+    onlyLetter($event) {
+      let charCode = $event.keyCode ? $event.keyCode : $event.which;
+      if (
+        !(
+          (charCode > 64 && charCode < 91) ||
+          (charCode > 96 && charCode < 123) ||
+          charCode == 32 || charCode == 46 || charCode ==39
+        )
+      ) {
+        $event.preventDefault();
+      }
+    }
+  }
 };
 </script>
 <style>
